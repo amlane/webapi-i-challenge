e55770f9c2cbcfc1af13176cde357dbae68639cb
amlane <twelvexstring@gmail.com>
2019-05-28 16:12:21 -0500

    hello world coming from get request

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+    
+})
+
+server.listen(8000, (req, res) => {
+    console.log('api running on port 8000')
+})
