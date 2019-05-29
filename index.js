c938c1de78579e7de447dcd3a556e03028fc7a92
amlane <twelvexstring@gmail.com>
2019-05-28 21:50:25 -0500

    my very first solo get end point, rollin with the homies

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
 
+server.use(express.json()); //makes post and put work
+
 server.get('/', (req, res) => {
-    res.send('Hello World');
-    
+    res.send('Hello World!');
 })
+
+server.get('/api/users', (req, res) => {
+    db.find().then( users => {
+        res.status(200).json(users);
+    } ).catch( error => {
+        res.status(500).json({ message: 'error retrieving users' })
+    })
+})
+
+
+
 
 server.listen(8000, (req, res) => {
     console.log('api running on port 8000')
